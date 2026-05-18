768d6e933f6be79e50b13109e8a432ae692df27d
Avigail <ac05271285@gmail.com>
2026-05-18 22:25:40 +0300

    categoryController and categoryService

--- Backend/src/controllers/categoryController.js
+++ Backend/src/controllers/categoryController.js
@@ -1,1 +1,51 @@
-imp
+   const categoryService = require( '../services/categoryService.js');
+// Fetching all categories
+exports.getAllCategories = async (req, res) => {
+    try {
+        const categories = await categoryService.getAllCategories();
+        res.status(200).json({
+            success: true,
+            data: categories
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error fetching categories",
+            error: error.message
+        });
+    }
+};
+ 
+exports.getSubCategoriesByCategoryId = async (req, res) => {
+    try {
+        const categoryId = req.params.categoryId;
+        const subCategories = await categoryService.getSubCategoriesByCategoryId(categoryId);
+        res.status(200).json({
+            success: true,
+            data: subCategories
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error fetching subcategories",
+            error: error.message
+        });
+    }
+};
+// Fetching subCategory by ID
+exports.getSubCategoryById = async (req, res) => {
+    try {
+        const subCategoryId = req.params.subCategoryId;
+        const subCategory = await categoryService.getSubCategoryById(subCategoryId);
+        res.status(200).json({
+            success: true,
+            data: subCategory
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error fetching subcategory",
+            error: error.message
+        });
+    }
+};
--- Backend/src/services/categoryService.js
+++ Backend/src/services/categoryService.js
@@ -1,5 +1,5 @@
-const {Category} = require('../models');
-
+const {Category,SubCategory} = require('../models');
+// Fetching all categories
 exports.getAllCategories = async () => {
     try {
         const categories = await Category.findAll();
@@ -8,3 +8,23 @@ exports.getAllCategories = async () => {
         throw new Error('An error occurred while fetching categories.');
     }
 };
+
+exports.getSubCategoriesByCategoryId = async (categoryId) => {
+    try {
+        const category = await Category.findByPk(categoryId, {
+            include: 'subCategories'
+        });
+        return category;
+    } catch (error) {
+        throw new Error('An error occurred while fetching the category.');
+    }
+}
+// Fetching subCategory by ID 
+exports.getSubCategoryById = async (subCategoryId) => {
+    try {
+        const subCategory = await SubCategory.findByPk(subCategoryId);
+        return subCategory;
+    } catch (error) {
+        throw new Error('An error occurred while fetching the subCategory.');
+    }
+}
